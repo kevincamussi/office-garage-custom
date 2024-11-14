8a9562bb5818938c3429475150cb077872851362
kevincamussi <kevincamussi@gmail.com>
2024-11-14 12:19:01 -0300

    fix: img size

--- src/components/BackgroundCarousel/index.tsx
+++ src/components/BackgroundCarousel/index.tsx
@@ -8,12 +8,20 @@ export const BackgroundCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const images = [
-    "/carousel/foto4.JPG",
-    "/carousel/foto5.JPG",
-    "/carousel/foto6.JPG",
-    "/carousel/foto7.JPG",
-    "/carousel/foto8.JPG",
-    "/carousel/foto9.JPG",
+    "/carousel/FOTO1.JPG",
+    "/carousel/FOTO2.JPG",
+    "/carousel/FOTO3.JPG",
+    "/carousel/FOTO4.JPG",
+    "/carousel/FOTO5.JPG",
+    "/carousel/FOTO6.JPG",
+    "/carousel/FOTO7.JPG",
+    "/carousel/FOTO8.JPG",
+    "/carousel/FOTO9.JPG",
+    "/carousel/FOTO10.JPG",
+    "/carousel/FOTO11.JPG",
+    "/carousel/FOTO12.JPG",
+    "/carousel/FOTO13.JPG",
+    "/carousel/FOTO14.JPG",
   ];
 
   useEffect(() => {
@@ -39,7 +47,7 @@ export const BackgroundCarousel = () => {
         <Image
           layout="fill"
           objectFit="cover"
-          priority
+          priority={index === 0}
           key={index}
           src={src}
           alt={`Background ${index + 1}`}
